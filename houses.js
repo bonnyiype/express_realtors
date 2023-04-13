9088b28d1a37a682727a6469cedb107a22a71dda
BONNY IYPE <105000155+bonnyiype@users.noreply.github.com>
2023-04-13 01:11:01 +0100

    Update houses.js

--- houses.js
+++ houses.js
@@ -1,1 +1,34 @@
 
+const houses = [
+    {
+      address: '1 Acacia Avenue',
+      city: 'London',
+      postcode: 'E1 7AA',
+      numberOfBedrooms: 3,
+      numberOfBathrooms: 2,
+      hasPorch: true,
+      price: 500000,
+    },
+    {
+      address: '21 Elm Street',
+      city: 'Manchester',
+      postcode: 'M16 0DF',
+      numberOfBedrooms: 4,
+      numberOfBathrooms: 2,
+      hasPorch: false,
+      price: 420000,
+    },
+    {
+      address: '10 Oak Lane',
+      city: 'Edinburgh',
+      postcode: 'EH11 2QA',
+      numberOfBedrooms: 2,
+      numberOfBathrooms: 1,
+      hasPorch: true,
+      price: 300000,
+    },
+
+  ];
+  
+  module.exports = houses;
+  
