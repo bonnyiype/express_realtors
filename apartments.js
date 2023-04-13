5e2fc53acc8a570630102729f77badb351891d87
BONNY IYPE <105000155+bonnyiype@users.noreply.github.com>
2023-04-13 01:10:16 +0100

    Create apartments.js

---
+++ apartments.js
@@ -0,0 +1,43 @@
+const apartments = [
+    {
+      address: '1 Oxford Street',
+      city: 'London',
+      postcode: 'W1D 2DA',
+      sqFt: 750,
+      price: 1500,
+      isPetFriendly: true,
+      hasGarden: false
+    },
+    {
+      address: '17 Market Square',
+      city: 'Manchester',
+      postcode: 'M1 1PD',
+      sqFt: 900,
+      price: 1200,
+      isPetFriendly: false,
+      hasGarden: true
+    },
+    {
+      address: '5 Broad Street',
+      city: 'Birmingham',
+      postcode: 'B1 2DN',
+      sqFt: 1100,
+      price: 1800,
+      isPetFriendly: true,
+      hasGarden: false
+    },
+    {
+      address: '10 Princes Street',
+      city: 'Edinburgh',
+      postcode: 'EH2 2AN',
+      sqFt: 800,
+      price: 1400,
+      isPetFriendly: true,
+      hasGarden: true
+    },
+
+  ];
+  
+  module.exports = apartments;
+  
+  
